319d43ee8fd92b67233df7ba9937f0e337fa1ae3
donghuizhou <765966797@qq.com>
2018-06-07 22:10:18 +0800

    finish

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { DragDropContext } from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
 import PropTypes from 'prop-types';
 import Square from './Square';
 import Knight from './Knight';
+import BoardSquare from './BoardSquare';
 import { moveKnight, canMoveKnight } from './Game';
 
-export default class Board extends Component {
+ class Board extends Component {
   handleSquareClick (toX, toY) {
     if (canMoveKnight(toX, toY)) {
       moveKnight(toX, toY);
@@ -13,19 +16,24 @@ export default class Board extends Component {
   renderSquare (i) {
     const x = i % 8;
     const y = Math.floor(i / 8);
-    const black = (x + y) % 2 === 1;
+    // const black = (x + y) % 2 === 1;
 
-    const [knightX, knightY] = this.props.knightPosition;
-    const piece = (x === knightX && y === knightY) ? <Knight /> : null;
+    // const [knightX, knightY] = this.props.knightPosition;
+    // const piece = (x === knightX && y === knightY) ? <Knight /> : null;
 
     return (
-      <div key={i} style={{ width: '12.5%', height: '12.5%' }}
-        onClick={() => this.handleSquareClick(x, y)}>
-        <Square black={black}>
-          {piece}
-        </Square>
+      <div key={i} style={{ width: '12.5%', height: '12.5%' }}>
+        <BoardSquare x={x} y={y}>
+          {this.renderPieces(x, y)}
+        </BoardSquare>
       </div>
     );
+  }
+  renderPieces (x, y) {
+    const [knightX, knightY] = this.props.knightPosition;
+    if (x === knightX && y === knightY) {
+      return <Knight />;
+    }
   }
   render () {
     const squares = [];
@@ -43,3 +51,5 @@ export default class Board extends Component {
 Board.propTypes = {
   knightPosition: PropTypes.arrayOf(PropTypes.number.isRequired).isRequired
 };
+
+export default DragDropContext(HTML5Backend)(Board);
